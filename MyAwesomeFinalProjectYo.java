9598aa22e107fe2073326c447da33005e5d3a38f
justinstrauss <Justin@Justins-MacBook-Pro.local>
2014-01-09 20:41:47 -0500

    commit uno

---
+++ MyAwesomeFinalProjectYo.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class MyAwesomeFinalProjectYo {
+    public MyAwesomeFinalProjectYo(){
+    }
+    public static void main(String[] args) {
+	MyAwesomeFinalProjectYo mastermind = new MyAwesomeFinalProjectYo();
+    }
+}
