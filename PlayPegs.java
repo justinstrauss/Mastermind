34c6676c0f028cb6d16ab3b1546c7cd8e0e12c31
Justin Strauss <justinstrauss@users.noreply.github.com>
2014-01-24 02:32:22 -0500

    Update PlayPegs.java

--- PlayPegs.java
+++ PlayPegs.java
@@ -6,25 +6,24 @@ public class PlayPegs extends Pegs{
     private static final String[] USED_COLORS = {"RED","BLUE","GREEN","YELLOW","PURPLE","BLACK"};
 
     public PlayPegs(){
-	_color = USED_COLORS[(int)(6 * Math.random())];
-	colorSpacing(6);
+        _color = USED_COLORS[(int)(6 * Math.random())];
+        colorSpacing(6);
     }
     
     public PlayPegs(int color){
-	_color = USED_COLORS[color];
-	colorSpacing(6);
-	//robustify
+        _color = USED_COLORS[color];
+        colorSpacing(6);
+        //robustify
     }
 
     public boolean equals(PlayPegs p){
-	return (p._color.equals(_color));
+        return (p._color.equals(_color));
     }
 
     /*    public static void main(String[] args){
-	PlayPegs p = new PlayPegs();
-	System.out.println(p);
-	System.out.println(p._color.length());
-	}*/
+        PlayPegs p = new PlayPegs();
+        System.out.println(p);
+        System.out.println(p._color.length());
+        }*/
 
 }
-
