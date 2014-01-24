0e89d56fea0ec8fc46768832c8dd8cb2e77d99de
Justin Strauss <justinstrauss@users.noreply.github.com>
2014-01-24 02:31:40 -0500

    Update Indicator.java

--- Indicator.java
+++ Indicator.java
@@ -6,13 +6,13 @@ public class Indicator extends Pegs{
     private static final String[] USED_COLORS = {"RED","WHITE"};
 
     public Indicator(String color){
-	String tColor = color.toUpperCase();
-	for (String x : USED_COLORS){
-	    if (x.equals(tColor)){
-		_color = tColor;
-	    }
-	}
-	colorSpacing(5);
+        String tColor = color.toUpperCase();
+        for (String x : USED_COLORS){
+            if (x.equals(tColor)){
+                _color = tColor;
+            }
+        }
+        colorSpacing(5);
     }
     
 }
