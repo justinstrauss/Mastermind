f9cfa54c14fad5ffda7e8529fa77ba9cdff57908
Justin Strauss <justinstrauss@users.noreply.github.com>
2014-01-24 02:32:02 -0500

    Update Pegs.java

--- Pegs.java
+++ Pegs.java
@@ -5,36 +5,35 @@ public class Pegs{
     protected String _color;
 
     public Pegs(){
-	_color = "NONE";
+        _color = "NONE";
     }
 
     public Pegs(String color){
-	_color = color.toUpperCase();
+        _color = color.toUpperCase();
     }
 
     public String getColor(){
-	return _color;
+        return _color;
     }
 
     public void setColor(String color){
-	_color = color.toUpperCase();
+        _color = color.toUpperCase();
     }
 
     public boolean equals(Pegs p){
-	return _color == p._color;
+        return _color == p._color;
     }
 
     public String toString(){
-	return _color;
+        return _color;
     }
 
     public void colorSpacing(int length){
-	String tS = "";
-	for(int x = 0; x < (length - _color.length()); x++){
-	    tS += " ";
-	}
-	setColor(_color + tS);
+        String tS = "";
+        for(int x = 0; x < (length - _color.length()); x++){
+            tS += " ";
+        }
+        setColor(_color + tS);
     }
 
 }
-
